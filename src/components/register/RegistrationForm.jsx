1c029be4443d6c45d28e2450254feee0c1db67bc
md shamsher <mdsamsher530@gmail.com>
2024-06-07 19:18:20 +0530

    phone require remove

--- src/components/register/RegistrationForm.jsx
+++ src/components/register/RegistrationForm.jsx
@@ -183,7 +183,7 @@ const RegistrationForm = () => {
                     id="phone"
                     placeholder="Enter Phone Number"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                    required={true}
+                    // required={true}
                     onChange={(e) =>
                       setFormData({
                         ...formData,
