c7ebddcf146b7c2fc336f87e5b4b395bd19c2c71
md shamsher <mdsamsher530@gmail.com>
2024-07-06 16:03:37 +0530

    new file added

--- src/app/messageList/page.jsx
+++ src/app/messageList/page.jsx
@@ -6,24 +6,29 @@ import axios from "axios";
 
 const Page = () => {
   const { user } = useContext(AuthContext);
-  console.log(user);
   const [messages, setMessages] = useState([]);
-  const [userId, setUserId] = useState("6688d33435c99e0937ea749c");
+  const [userId, setUserId] = useState("");
+
+  useEffect(() => {
+    if (user && user._id) {
+      setUserId(user._id);
+    }
+  }, [user]);
 
   useEffect(() => {
     const fetchMessages = async () => {
-      try {
-        const response = await axios.get(`/api/getMessage?userId=${userId}`);
+      if (userId) {
+        try {
+          const response = await axios.get(`/api/getMessage?userId=${userId}`);
 
-        console.log(response);
+          if (!response.data.success) {
+            throw new Error("Failed to fetch messages.");
+          }
 
-        if (!response.data.success) {
-          throw new Error("Failed to fetch messages.");
+          setMessages(response.data.data);
+        } catch (error) {
+          console.error("Error fetching messages:", error.message);
         }
-
-        setMessages(response.data.data);
-      } catch (error) {
-        console.error("Error fetching messages:", error.message);
       }
     };
 
@@ -61,8 +66,8 @@ const Page = () => {
         <ul>
           {messages?.map((message) => (
             <li key={message._id}>
-              <p>Sender: {message.sender.fullName}</p>
-              <p>Receiver: {message.receiver.fullName}</p>
+              <p>Sender: {message.sender}</p>
+              <p>Receiver: {message.receiver}</p>
               <p>Message: {message.message}</p>
               <p>Confirmed: {message.confirmed ? "Yes" : "No"}</p>
               {!message.confirmed && (
