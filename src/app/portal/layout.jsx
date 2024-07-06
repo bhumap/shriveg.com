9e1ecbd441e97d0fc5736cce4691eb896ce295f3
md shamsher <mdsamsher530@gmail.com>
2024-07-06 16:23:56 +0530

    added

--- src/app/portal/layout.jsx
+++ src/app/portal/layout.jsx
@@ -39,6 +39,11 @@ const Layout = ({ children }) => {
       href: "/portal/availability",
     },
 
+    user?.userType === "Chef" && {
+      label: "Push",
+      href: "/portal/message",
+    },
+
     user?.userType === "Delivery_Boy" && {
       label: "My Oders",
       href: "/messageList",
