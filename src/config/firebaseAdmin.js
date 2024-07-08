b0b69660eaa58d3de5682a14f2a8f2337671339a
md shamsher <mdsamsher530@gmail.com>
2024-07-08 18:19:35 +0530

    added

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,5 +1,5 @@
 import admin from 'firebase-admin';
-import serviceAccount from '@/config/@/config/shriveg-eb7c3-firebase-adminsdk-4utrd-b8462cb86e.json'; // Update this path with the actual path to your service account JSON file
+import serviceAccount from '/config/shriveg-eb7c3-firebase-adminsdk-4utrd-b8462cb86e.json'; // Update this path with the actual path to your service account JSON file
 
 if (!admin.apps.length) {
   admin.initializeApp({
