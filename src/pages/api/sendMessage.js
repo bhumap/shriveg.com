a375d7f0637256000eee657d832c519bd2aaa082
md shamsher <mdsamsher530@gmail.com>
2024-07-09 11:08:48 +0530

    push

--- src/pages/api/sendMessage.js
+++ src/pages/api/sendMessage.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
 
   if (req.method === 'POST') {
     try {
-      const { senderId, message, confirmedBy, address_Id } = req.body;
+      const { senderId, message, confirmedBy ,address_Id } = req.body;
 
       // Find sender
       const sender = await UsersModel.findById(senderId);
@@ -71,7 +71,6 @@ export default async function handler(req, res) {
             receiver: receiver._id,
             message,
             address_Id,
-   
             confirmedBy: confirmedBy,
             UniqueId: uniqueId, // Assign the unique ID to each message
           });
