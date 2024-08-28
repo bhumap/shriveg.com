8d84ae8c59c06513b55e21dfe74776c8022adc26
11ashu <srivastavashutosh80@gmail.com>
2024-08-28 11:41:47 +0530

    CHANGE CONTACT US PAGE DESIGN

--- src/app/ContactUs/page.jsx
+++ src/app/ContactUs/page.jsx
@@ -6,51 +6,94 @@ const Contact = () => {
   return (
     <div>
       <section className="bg-gray-50">
-        <div className="flex flex-col items-center justify-center px-4 sm:px-10 md:px-16 lg:px-28 py-10 mx-auto">
-          <article className="text-pretty">
-          <h2 className="text-2xl sm:text-3xl font-semibold mb-6 text-gray-800 text-center">Contact Us</h2>
-            <div className="flex justify-center items-center mt-5 bg-gray-100">
-              <div className="bg-white shadow-lg rounded-lg p-8 max-w-md">
-               
+        <div className="flex flex-col items-center justify-center px-4 sm:px-10 md:px-16 lg:px-28 py-16  mx-auto">
+        <h3 className="text-xl sm:text-2xl text-center mb-6 sm:mb-10 tracking-wide">CONTACT US </h3>
+          <div className="max-w-screen-lg mx-auto p-5">
+            <div className="grid grid-cols-1 md:grid-cols-12 border">
+              <div className="md:col-span-4 p-10 text-white bg-baseColor">
+                <p className="mt-4 text-sm leading-7 font-regular uppercase">
+                  Contact
+                </p>
+                <h3 className="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight">
+                  Get In <span>Touch</span>
+                </h3>
+                <p className="mt-4 leading-7 text-gray-200">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s.
+                </p>
 
-                <div className="text-sm sm:text-base font-light tracking-wide leading-relaxed text-gray-600 space-y-6">
-                  <div className="flex items-center space-x-4">
-                  <svg class="h-6 w-8" color='rgb(6 89 18 / var(--tw-bg-opacity))'  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"/>
+                <div className="flex items-center mt-5">
+                  <svg className="h-6 mr-2" fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 489.536 489.536" xmlnsXlink="http://www.w3.org/1999/xlink" enableBackground="new 0 0 489.536 489.536">
+                    <g>
+                      <g>
+                        <path d="m488.554,476l-99-280.2c-1-4.2-5.2-7.3-9.4-7.3h-45.6c12.9-31.1 19.6-54.9 19.6-70.8 0-64.6-50-117.7-112.5-117.7-61.5,0-112.5,52.1-112.5,117.7 0,17.6 8.2,43.1 19.9,70.8h-39.7c-4.2,0-8.3,3.1-9.4,7.3l-99,280.2c-3.2,10.3 6.3,13.5 9.4,13.5h468.8c4.2,0.5 12.5-4.2 9.4-13.5zm-246.9-455.3c51,1.06581e-14 91.7,43.7 91.7,96.9 0,56.5-79.2,182.3-91.7,203.1-31.3-53.1-91.7-161.5-91.7-203.1 0-53.1 40.6-96.9 91.7-96.9zm-216.7,448l91.7-259.4h41.7c29.9,64.1 83.3,151 83.3,151s81.4-145.7 83.8-151h47.4l91.7,259.4h-439.6z" />
+                        <rect width="136.5" x="177.054" y="379.1" height="20.8" />
+                        <path d="m289.554,108.2c0-26-21.9-47.9-47.9-47.9s-47.9,21.9-47.9,47.9 20.8,47.9 47.9,47.9c27.1,0 47.9-21.8 47.9-47.9zm-75-1c0-14.6 11.5-27.1 27.1-27.1s27.1,12.5 27.1,27.1-11.5,27.1-27.1,27.1c-14.6,2.84217e-14-27.1-12.5-27.1-27.1z" />
+                      </g>
+                    </g>
                   </svg>
-                    <div>
-                      <p className="font-medium text-lg sm:text-base">Company Name</p>
-                      <p>Shriveg Homemade Service Pvt Ltd</p>
-                    </div>
-                  </div>
+                  <span className="text-sm">House #14, Street #12, Hazaratganj, Lucknow, Uttar Pradesh.</span>
+                </div>
+                <div className="flex items-center mt-5">
+                  <svg className="h-6 mr-2" fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 60.002 60.002" style={{ enableBackground: "new 0 0 60.002 60.002" }} xmlSpace="preserve">
+                    <g>
+                      <path d="M59.002,37.992c-3.69,0-6.693-3.003-6.693-6.693c0-0.553-0.447-1-1-1s-1,0.447-1,1c0,4.794,3.899,8.693,8.693,8.693c0.553,0,1-0.447,1-1S59.554,37.992,59.002,37.992z" />
+                      <path d="M58.256,35.65c0.553,0,1-0.447,1-1s-0.447-1-1-1c-0.886,0-1.605-0.72-1.605-1.605c0-0.553-0.447-1-1-1s-1,0.447-1,1C54.65,34.033,56.267,35.65,58.256,35.65z" />
+                      <path d="M20.002,2.992c3.691,0,6.693,3.003,6.693,6.693c0,0.553,0.448,1,1,1s1-0.447,1-1c0-4.794-3.9-8.693-8.693-8.693c-0.552,0-1,0.447-1,1S19.449,2.992,20.002,2.992z" />
+                      <path d="M19.748,6.334c0,0.553,0.448,1,1,1c0.885,0,1.604,0.72,1.604,1.605c0,0.553,0.448,1,1,1s1-0.447,1-1c0-1.988-1.617-3.605-3.604-3.605C20.196,5.334,19.748,5.781,19.748,6.334z" />
+                      <path d="M44.076,37.889c-1.276-0.728-2.597-0.958-3.721-0.646c-0.844,0.234-1.532,0.768-1.996,1.546c-1.02,1.22-2.286,2.646-2.592,2.867c-2.367,1.604-4.25,1.415-6.294-0.629L17.987,29.542c-2.045-2.045-2.233-3.928-0.631-6.291c0.224-0.31,1.65-1.575,2.87-2.596c0.778-0.464,1.312-1.152,1.546-1.996c0.311-1.123,0.082-2.444-0.652-3.731c-0.173-0.296-4.291-7.27-8.085-9.277c-1.926-1.019-4.255-0.669-5.796,0.872L4.7,9.059c-4.014,4.014-5.467,8.563-4.321,13.52c0.956,4.132,3.742,8.529,8.282,13.068l14.705,14.706c5.762,5.762,11.258,8.656,16.298,8.656c3.701,0,7.157-1.562,10.291-4.695l2.537-2.537c1.541-1.541,1.892-3.87,0.872-5.796C51.356,42.186,44.383,38.069,44.076,37.889z M51.078,50.363L48.541,52.9c-6.569,6.567-14.563,5.235-23.76-3.961L10.075,34.232c-4.134-4.134-6.705-8.048-7.576-11.874c-1.005-4.348,0.176-8.366,3.504-11.695l2.537-2.537c1.057-1.057,2.64-1.298,3.973-0.617c3.212,1.7,7.185,7.764,7.344,8.034c0.903,1.54,1.014,2.797,0.336,3.781c-0.289,0.415-1.79,1.633-2.925,2.614c-3.061,2.722-3.276,5.979-0.686,8.569l11.439,11.439c2.591,2.591,5.846,2.375,8.569-0.686c0.98-1.136,2.198-2.637,2.614-2.926c0.984-0.678,2.241-0.566,3.781,0.336c0.27,0.158,6.334,4.132,8.033,7.344C52.375,47.724,52.134,49.307,51.078,50.363z" />
+                    </g>
+                  </svg>
+                  <span className="text-sm">+91 78000 09965</span>
+                </div>
+                <div className="flex items-center mt-5">
+                  <svg
+                    className="h-6 mr-2"
+                    fill="currentColor"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 12.713l11.985-8.713H0.015L12 12.713zm11.985-7.713v12h-24v-12l12 8.571L23.985 5z" />
+                  </svg>
 
-                  <div className="flex items-center space-x-4">
-                  <svg class="h-6 w-8" color='rgb(6 89 18 / var(--tw-bg-opacity))'  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="5 12 3 12 12 3 21 12 19 12" />  <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />  <rect x="10" y="12" width="4" height="4" /></svg>
-                    <div>
-                      <p className="font-medium text-lg sm:text-base">Company Address</p>
-                      <p>123 Business Avenue, Suite 456,<br /> City, State, ZIP Code, Country</p>
-                    </div>
-                  </div>
-
-                  <div className="flex items-center space-x-4">
-                  <svg class="h-6 w-8" color='rgb(6 89 18 / var(--tw-bg-opacity))'  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" /></svg>
-                    <div>
-                      <p className="font-medium text-lg sm:text-base">Phone No</p>
-                      <p>78000 09965</p>
-                    </div>
-                  </div>
-
-                  <div className="flex items-center space-x-4">
-                  <svg class="h-6 w-8" color='rgb(6 89 18 / var(--tw-bg-opacity))'  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <rect x="3" y="5" width="18" height="14" rx="2" />  <polyline points="3 7 12 13 21 7" /></svg>
-                    <div>
-                      <p className="font-medium text-lg sm:text-base">Email Id</p>
-                      <p>shrivegofficial@gmail.com</p>
-                    </div>
-                  </div>
+                  <span className="text-sm">shrivegofficial@gmail.com</span>
                 </div>
               </div>
+
+              <div className="md:col-span-8 p-10">
+                <form className="flex flex-col justify-center w-full">
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                  <input
+                    placeholder="First Name"
+                    type="text"
+                    className="p-4 border border-gray-300 focus:border-transparent focus:outline-none focus:ring-0 text-sm"
+                  />
+                    <input
+                      placeholder="Last Name"
+                      type="text"
+                      className="p-4 border border-gray-300 focus:border-transparent focus:outline-none focus:ring-0 text-sm"
+                    />
+                  </div>
+                  <input
+                    placeholder="Email"
+                    type="text"
+                    className="p-4 border border-gray-300 outline-none text-sm mt-5  focus:border-transparent focus:outline-none focus:ring-0"
+                  />
+                  <textarea
+                    rows="10"
+                    placeholder="Message"
+                    className="p-4 border border-gray-300 outline-none text-sm mt-5  focus:border-transparent focus:outline-none focus:ring-0"
+                  ></textarea>
+                  <button
+                    type="submit"
+                    className="py-4 px-6 bg-baseColor text-white font-bold rounded-lg mt-5 text-sm"
+                  >
+                    Send Message
+                  </button>
+                </form>
+              </div>
             </div>
-          </article>
+          </div>
         </div>
       </section>
     </div>
